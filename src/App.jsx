24757365b164c0ba564bc099668b5b155e399b85
Jonathan Melitski <jonathanmelitski@gmail.com>
2024-12-24 22:35:42 -0500

    initial commit

---
+++ src/App.jsx
@@ -0,0 +1,13 @@
+function App() {
+  return (
+    <>
+      <div className="flex min-h-screen bg-felt align-center justify-center">
+        <div className="text-white text-3xl">
+          POKE HER
+        </div>
+      </div>
+    </>
+  )
+}
+
+export default App
---
+++ tailwind.config.js
@@ -0,0 +1,15 @@
+/** @type {import('tailwindcss').Config} */
+export default {
+  content: [
+    "./index.html",
+    "./src/**/*.{js,ts,jsx,tsx}",
+  ],
+  theme: {
+    colors: {
+      "felt":"#0c5c00",
+    },
+    extend: {},
+  },
+  plugins: [],
+}
+
